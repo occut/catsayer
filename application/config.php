89b0e3632ff9390503102cd4e511f25239636b8b
ChinaLeng <woaini3900098@qq.com>
2017-11-12 13:08:28 +0800

    //配置

--- application/config.php
+++ application/config.php
@@ -125,7 +125,7 @@ return [
         // 模板引擎类型 支持 php think 支持扩展
         'type'         => 'Think',
         // 模板路径
-        'view_path'    => '',
+        'view_path'    => __DIR__ . '/../Template/',
         // 模板后缀
         'view_suffix'  => 'html',
         // 模板文件名分隔符
@@ -141,7 +141,9 @@ return [
     ],
 
     // 视图输出字符串内容替换
-    'view_replace_str'       => [],
+    'view_replace_str'       => [
+        '__INDEX__' => '/public/Index',
+    ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
     'dispatch_error_tmpl'    => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
